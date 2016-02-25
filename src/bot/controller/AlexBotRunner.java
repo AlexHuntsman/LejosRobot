602c7ada2248c729a6c17883a8a143bebeec2b01
AlexHuntsman <ahun0442@740s-pl-118.local>
2016-02-25 10:03:17 -0700

    Fixed ledges and uploaded basic robot

---
+++ src/bot/controller/AlexBotRunner.java
@@ -0,0 +1,13 @@
+package bot.controller;
+
+import bot.controller.BotController;
+public class AlexBotRunner 
+{
+
+	public static void main(String[] args) 
+	{
+		BotController myBotController = new BotController();
+		myBotController.start();
+	}
+
+}
---
+++ src/bot/model/EV3Bot.java
@@ -0,0 +1,26 @@
+package bot.model;
+
+import lejos.hardware.ev3.LocalEV3;
+import lejos.hardware.lcd.LCD;
+import lejos.hardware.motor.Motor;
+import lejos.hardware.port.Port;
+import lejos.robotics.chassis.Wheel;
+import lejos.robotics.chassis.WheeledChassis;
+import lejos.robotics.navigation.MovePilot;
+import lejos.utility.Delay;
+import lejos.hardware.sensor.EV3TouchSensor;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+
+public class EV3Bot 
+{
+	
+	public void driveRoom()
+	{
+		
+	}
+	
+	private void displayMessage(String message)
+	{
+		
+	}
+}
